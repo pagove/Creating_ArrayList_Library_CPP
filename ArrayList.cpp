a16e91d0f0a47ac543009a1fa61cd27ad6dcd906
DESKTOP-A0E1H14\pablo <pablogomve@gmail.com>
2025-01-05 00:05:13 +0100

    Añadiendo más funcionalidades

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -3,253 +3,313 @@
 using namespace std;
 
 template <typename T>
-class Node {
-    public:
-        Node<T>* previous;
-        Node<T>* next;
-        T value;
-        //Constructor
-        Node(): previous(nullptr), next(nullptr){};
+class Node
+{
+public:
+    Node<T> *previous;
+    Node<T> *next;
+    T value;
+    // Constructor
+    Node() : previous(nullptr), next(nullptr) {};
 
-        T print() {
-            return value;
-        }
+    T print()
+    {
+        return value;
+    }
 
-    private:
-
+private:
 };
 
 template <typename T>
-class ArrayList{
-    public:
+class ArrayList
+{
+public:
+    ArrayList() : _size(0), head(nullptr), tail(nullptr) {};
 
-        ArrayList(): _size(0), head(nullptr), tail(nullptr){};
-
-        /*ADD METHODS*/
-        void add(T val){
-            Node<T>* newNode = new Node<T>();
-            newNode->value = val;
-            if(_size == 0){
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail->next = newNode;
-                newNode->previous = tail;
-                tail = newNode;
-            }
-            _size++;
+    /*ADD METHODS*/
+    void add(T val)
+    {
+        Node<T> *newNode = new Node<T>();
+        newNode->value = val;
+        if (_size == 0)
+        {
+            head = newNode;
+            tail = newNode;
         }
-
-        void addAll(ArrayList<T> ar){
-            Node<T>* current = ar.head;
-            while(current){
-                add(current->value);
-                current = current->next;
-            }
+        else
+        {
+            tail->next = newNode;
+            newNode->previous = tail;
+            tail = newNode;
         }
+        _size++;
+    }
 
-        void set(int index, T val){
-            if(index < _size && index >= 0){
-                int med = _size / 2;
-                Node<T>* current;
-                if(index <= med){
-                    current = head;
-                    for(int i = 0; i < index; i++){
-                        cout << current->value << endl;
-                        current = current->next;
-                    }
-                } else {
-                    current = tail;
-                    for(int i = _size -1; i > index; i--){
-                        cout << current->value << endl;
-                        current = current->previous;
-                    }
-                }
-                current->value = val;
-            }
+    void addAll(ArrayList<T> ar)
+    {
+        Node<T> *current = ar.head;
+        while (current)
+        {
+            add(current->value);
+            current = current->next;
         }
+    }
 
-
-        /* REMOVE METHODS*/
-
-        void remove(T val){
-            Node<T>* current = head;
-            bool found = false;
-            int pos = -1;
-            while(current && !found ){
-                pos++;
-                if(current->value == val){
-                    found=true;
-                } else {
-                    current = current->next;
-                }
-            }
-            if(found){
-                if(pos == 0){
-                    head = current->next;
-                    head->previous = nullptr;
-                } else if(pos == (_size -1)){
-                    tail = current->previous;
-                    tail->next = nullptr;
-                } else {
-                    current->previous->next = current->next;
-                    current->next->previous = current->previous;
-                }
-                delete current;
-            }
-            _size--;
-        }
-
-        void removeAll(T val){
-            Node<T>* current = head;
-            int pos = -1;
-            while(current){
-                pos++;
-                if(current->value == val){
-                    Node<T>* nextNode = current;
-                    if(pos == 0){
-                        head = current->next;
-                    }else if (pos == _size -1){
-                        tail = current->previous;
-                    }
-                    delete current;
-                    current = nextNode;
-                    _size--;
-                }
-            }
-        }
-
-        void removeAt(int index){
-            int cont = 0;
-            Node<T>* current = head;
-            if(index == 0){
-                head = current->next;
-                head->previous = nullptr;
-                delete current;
-            } else if(index == (_size - 1)) {
-                current = tail;
-                tail = current->previous;
-                tail->next = nullptr;
-                delete current;
-            } else {
-                while(current->next){
-                    if(cont == index){
-                        current->previous->next = current->next;
-                        current->next->previous = current->previous;
-                        delete(current);
-                        break;
-                    }
-                    current = current->next;
-                    cont++;
-                }
-            }
-            _size--;
-        }
-
-        void clear() {
-            Node<T>* current = head;
-            while (current) {
-                Node<T>* nextNode = current->next;
-                delete current;
-                current = nextNode;
-            }
-            head = tail = nullptr;
-            _size = 0;
-        }
-
-
-
-        /* ACCES METHODS*/
-        T getAt(int pos){
+    void set(int index, T val)
+    {
+        if (index < _size && index >= 0)
+        {
             int med = _size / 2;
-            Node<T>* current;
-            if(pos <= med){
-                 current = head;
-                for(int i = 0; i < pos; i++){
+            Node<T> *current;
+            if (index <= med)
+            {
+                current = head;
+                for (int i = 0; i < index; i++)
+                {
+                    cout << current->value << endl;
                     current = current->next;
                 }
-                return current->value;
-            } else {
+            }
+            else
+            {
                 current = tail;
-                for(int i = _size -1; i > pos; i--){
+                for (int i = _size - 1; i > index; i--)
+                {
+                    cout << current->value << endl;
                     current = current->previous;
                 }
-                return current->value;
             }
-            return false;
+            current->value = val;
         }
+    }
 
-        int indexOf(T val){
-            int index = -1;
-            Node<T>* current = head;
-            bool found = false;
-            while(current && !found){
-                index++;
-                if(current->value == val){
-                    found = true;
-                }
+    /* REMOVE METHODS*/
+
+    void remove(T val)
+    {
+        Node<T> *current = head;
+        bool found = false;
+        int pos = -1;
+        while (current && !found)
+        {
+            pos++;
+            if (current->value == val)
+            {
+                found = true;
+            }
+            else
+            {
                 current = current->next;
             }
-            if(found){
-                return index;
-            } else {
-                return -1;
+        }
+        if (found)
+        {
+            if (pos == 0)
+            {
+                head = current->next;
+                head->previous = nullptr;
+            }
+            else if (pos == (_size - 1))
+            {
+                tail = current->previous;
+                tail->next = nullptr;
+            }
+            else
+            {
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
+            }
+            delete current;
+        }
+        _size--;
+    }
+
+    void removeAll(T val)
+    {
+        Node<T> *current = head;
+        int pos = -1;
+        while (current)
+        {
+            pos++;
+            if (current->value == val)
+            {
+                Node<T> *nextNode = current;
+                if (pos == 0)
+                {
+                    head = current->next;
+                }
+                else if (pos == _size - 1)
+                {
+                    tail = current->previous;
+                }
+                delete current;
+                current = nextNode;
+                _size--;
             }
         }
+    }
 
-        bool contains(T val){
-            Node<T>* current  = head;
-            bool found = false;
-            while(current && !found){
-                if(current->value == val) {
-                    found = true;
+    void removeAt(int index)
+    {
+        int cont = 0;
+        Node<T> *current = head;
+        if (index == 0)
+        {
+            head = current->next;
+            head->previous = nullptr;
+            delete current;
+        }
+        else if (index == (_size - 1))
+        {
+            current = tail;
+            tail = current->previous;
+            tail->next = nullptr;
+            delete current;
+        }
+        else
+        {
+            while (current->next)
+            {
+                if (cont == index)
+                {
+                    current->previous->next = current->next;
+                    current->next->previous = current->previous;
+                    delete (current);
+                    break;
                 }
                 current = current->next;
-            }
-
-            return found;
-        }
-
-        int size(){
-            return _size;
-        }
-        bool isEmpty(){
-            return _size == 0;
-        }
-
-        string print() {
-            string ret = "";
-            Node<T>* current = head;
-            while(current){
-                if(current->next != nullptr){
-                    ret += to_string(current->print()) + "-";
-                } else {
-                    ret += to_string(current->print());
-                }
-                current = current->next;
-            }
-            return ret;
-        }
-
-        ArrayList<T> subList(int fromIndex, int toIndex){
-            int cont = -1;
-            ArrayList<T> rest;
-            Node<T>* current = head;
-            while(current->next && cont < toIndex){
                 cont++;
-                if(cont >= fromIndex && cont <= toIndex){
-                    rest.add(current->value);
-                }
+            }
+        }
+        _size--;
+    }
+
+    void clear()
+    {
+        Node<T> *current = head;
+        while (current)
+        {
+            Node<T> *nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+        head = tail = nullptr;
+        _size = 0;
+    }
+
+    /* ACCES METHODS*/
+    T getAt(int pos)
+    {
+        int med = _size / 2;
+        Node<T> *current;
+        if (pos <= med)
+        {
+            current = head;
+            for (int i = 0; i < pos; i++)
+            {
                 current = current->next;
             }
-            return rest;
+            return current->value;
+        }
+        else
+        {
+            current = tail;
+            for (int i = _size - 1; i > pos; i--)
+            {
+                current = current->previous;
+            }
+            return current->value;
+        }
+        return false;
+    }
+
+    int indexOf(T val)
+    {
+        int index = -1;
+        Node<T> *current = head;
+        bool found = false;
+        while (current && !found)
+        {
+            index++;
+            if (current->value == val)
+            {
+                found = true;
+            }
+            current = current->next;
+        }
+        if (found)
+        {
+            return index;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    bool contains(T val)
+    {
+        Node<T> *current = head;
+        bool found = false;
+        while (current && !found)
+        {
+            if (current->value == val)
+            {
+                found = true;
+            }
+            current = current->next;
         }
 
+        return found;
+    }
 
-    private:
-        int _size;
-        Node<T>* head;
-        Node<T>* tail;
+    int size()
+    {
+        return _size;
+    }
+    bool isEmpty()
+    {
+        return _size == 0;
+    }
+
+    string toString()
+    {
+        string ret = "";
+        Node<T> *current = head;
+        while (current)
+        {
+            if (current->next != nullptr)
+            {
+                ret += to_string(current->print()) + "-";
+            }
+            else
+            {
+                ret += to_string(current->print());
+            }
+            current = current->next;
+        }
+        return ret;
+    }
+
+    ArrayList<T> subList(int fromIndex, int toIndex)
+    {
+        int cont = -1;
+        ArrayList<T> rest;
+        Node<T> *current = head;
+        while (current->next && cont < toIndex)
+        {
+            cont++;
+            if (cont >= fromIndex && cont <= toIndex)
+            {
+                rest.add(current->value);
+            }
+            current = current->next;
+        }
+        return rest;
+    }
+
+private:
+    int _size;
+    Node<T> *head;
+    Node<T> *tail;
 };
-
-
--- Main.cpp
+++ Main.cpp
@@ -7,66 +7,130 @@ void clearTerminal();
 void pause();
 void howToUse();
 int menu();
+void optionsMenu(int &showArray, int &clearTerm);
+int saveDisplayArray();
+int clearTerm();
 
 void add(ArrayList<int> &v);
+void addAll(ArrayList<int> &v);
 void printAr(ArrayList<int> &v);
+void setValue(ArrayList<int> &v);
+void removeFirst(ArrayList<int> &v);
+void remove(ArrayList<int> &v);
+void removeAt(ArrayList<int> &v);
+void clear(ArrayList<int> &v);
+void getAt(ArrayList<int> &v);
+void contains(ArrayList<int> &v);
+void size(ArrayList<int> &v);
 
-int main() {
+int main()
+{
 
 	int option = -1;
+	int showArray = 0;
+	int clearTerm = 0;
 	ArrayList<int> v;
-	
-	while(option != 0){
+
+	while (option != 0)
+	{
 		option = menu();
-		switch(option){
-			case 0:
-				exit(0);
+
+		switch (option)
+		{
+		case 0:
+			exit(0);
 			break;
-			case 1:
-				add(v);
+		case 1:
+			add(v);
 			break;
-			case 14:
-				printAr();
+		case 2:
+			addAll(v);
 			break;
-			case 15:
-				howToUse();
+		case 3:
+			setValue(v);
 			break;
-			case 16:
-				clearTerminal();
+		case 4:
+			removeFirst(v);
+			break;
+		case 5:
+			remove(v);
+			break;
+		case 6:
+			removeAt(v);
+			break;
+		case 7:
+			clear(v);
+			break;
+		case 8:
+			getAt(v);
+			break;
+		case 9:
+			contains(v);
+			break;
+		case 10:
+			size(v);
+			break;
+		case 11:
+			break;
+		case 14:
+			printAr(v);
+			break;
+		case 15:
+			howToUse();
+			break;
+		case 16:
+			clearTerminal();
+			break;
+		case 17:
+			optionsMenu(showArray, clearTerm);
 			break;
 		}
-		if(option != 16){
+
+		if (option != 16)
+		{
+			if (showArray == 1 && option != 14 && option != 17)
+			{
+				printAr(v);
+			}
 			pause();
+			if (clearTerm == 1)
+			{
+				clearTerminal();
+			}
 		}
 	}
-	
-    return 0;
+
+	return 0;
 }
 
-void clearTerminal(){
-	#ifdef _WIN32
-		system("cls");
-	#else
-		system("clear");
-	#endif
+void clearTerminal()
+{
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
 }
 
-void pause(){
+void pause()
+{
 	char c;
 	cout << " Press 'C' to continue" << endl;
 	cin >> c;
 }
 
-int menu(){
+int menu()
+{
 	int option = -1;
-	while(option < 0 || option > 16){
+	while (option < 0 || option > 17)
+	{
 		cout << "=============================================" << endl;
 		cout << "                  MENU " << endl;
 		cout << "=============================================" << endl;
 		cout << " 0.-  Exit. " << endl;
 		cout << " 1.-  Add number to vector. " << endl;
-		cout << " 2.-  Add another array to main array. " << endl;
-		cout << " 3.-  Set value. " << endl;			
+		cout << " 2.-  Add another array to C array. " << endl;
+		cout << " 3.-  Set value. " << endl;
 		cout << " 4.-  Remove first value. " << endl;
 		cout << " 5.-  Remove all occurrences. " << endl;
 		cout << " 6.-  Remove at. " << endl;
@@ -80,22 +144,72 @@ int menu(){
 		cout << " 14.- print " << endl;
 		cout << " 15.- How to use " << endl;
 		cout << " 16.- Clear screen" << endl;
+		cout << " 17.- Option" << endl;
 		cout << "============================================" << endl;
-		
+
 		cin >> option;
-		
-		if(option < 0 || option > 16){
+
+		if (option < 0 || option > 17)
+		{
 			clearTerminal();
 		}
 	}
-	
+
 	return option;
 }
 
-void howToUse(){ 
+void optionsMenu(int &showArray, int &clearScreen)
+{
+	clearTerminal();
+	int option = -1;
+	cout << option << endl;
+	while (option < 0 || option > 2)
+	{
+		cout << "=============================================" << endl;
+		cout << "             OPTIONS MENU " << endl;
+		cout << "=============================================" << endl;
+		cout << " 0.-  Return to main menu. " << endl;
+		cout << " 1.-  Show array after each action. " << endl;
+		cout << " 2.-  Clear terminal after each action" << endl;
+		cout << "============================================" << endl;
+
+		cin >> option;
+
+		switch (option)
+		{
+		case 1:
+			showArray = saveDisplayArray();
+			break;
+		case 2:
+			clearScreen = clearTerm();
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+int saveDisplayArray()
+{
+	int val = 0;
+	cout << "Insert 1 to see the main array after each action " << endl;
+	cin >> val;
+	return val;
+}
+
+int clearTerm()
+{
+	int val = 0;
+	cout << "Insert 1 to clear the screen after each action " << endl;
+	cin >> val;
+	return val;
+}
+
+void howToUse()
+{
 	cout << "1.-   Add number to the main array. Add(T value). " << endl;
 	cout << "2.-   Add another array to the main array. Add(ArrayList<T> ar). " << endl;
-	cout << "3.-   Set a number in the indicated position. set(int index, T value). " << endl;
+	cout << "3.-   Set a number in the indicated position, like edit value. set(int index, T value). " << endl;
 	cout << "4.-   Remove the first occurrence. remove(T value). " << endl;
 	cout << "5.-   Remove all occurrences. removeAll(T value). " << endl;
 	cout << "6.-   Remove value. removeAt(int index). " << endl;
@@ -109,13 +223,157 @@ void howToUse(){
 	cout << "14.- Prints the array into screen. print()." << endl;
 }
 
-void add(ArrayList<int> &v){
+void add(ArrayList<int> &v)
+{
 	int val;
 	cout << "Insert a numeric value" << endl;
 	cin >> val;
 	v.add(val);
 }
 
-void printAr(ArrayList<int> &v){
-	cout << "[" << v.print() << "]" << endl;	
+void addAll(ArrayList<int> &v)
+{
+	string continuar = "S";
+	ArrayList<int> _v;
+	int aux;
+	cout << "Creating an auxiliar array" << endl;
+	while (continuar == "S")
+	{
+		cout << "Write a number: " << endl;
+		cin >> aux;
+		_v.add(aux);
+		cout << "Continue (S) or not (N) " << endl;
+		cin >> continuar;
+	}
+
+	v.addAll(_v);
+}
+
+void setValue(ArrayList<int> &v)
+{
+	int index, number;
+	bool ok = true;
+	cout << "Write the index of the array" << endl;
+	cin >> index;
+	if (index < 0)
+	{
+		cout << "Index could not be negative" << endl;
+		ok = false;
+	}
+	if (index > (v.size() - 1))
+	{
+		cout << "Index out of size. The size is " << v.size() << endl;
+		ok = false;
+	}
+
+	if (ok)
+	{
+		cout << "Insert a number to write in the position " << index << endl;
+		cin >> number;
+		v.set(index, number);
+	}
+}
+
+void removeFirst(ArrayList<int> &v)
+{
+	if (v.size() > 0)
+	{
+		cout << "Removing first value..." << endl;
+		v.removeAt(0);
+	}
+	else
+	{
+		cout << "You are trying to remove elements in an empty array " << endl;
+	}
+}
+
+void remove(ArrayList<int> &v)
+{
+	int val;
+	if (v.size() > 0)
+	{
+		cout << "Insert the value that you want to remove" << endl;
+		cin >> val;
+		v.remove(val);
+	}
+	else
+	{
+		cout << "You are trying to remove elements in an empty array " << endl;
+	}
+}
+
+void removeAt(ArrayList<int> &v)
+{
+	int index;
+	bool ok = true;
+	cout << "Write the index of the array" << endl;
+	cin >> index;
+	if (index < 0)
+	{
+		cout << "Index could not be negative" << endl;
+		ok = false;
+	}
+	if (index > (v.size() - 1))
+	{
+		cout << "Index out of size. The size is " << v.size() << endl;
+		ok = false;
+	}
+
+	if (ok)
+	{
+		v.removeAt(index);
+	}
+}
+
+void clear(ArrayList<int> &v)
+{
+	v.clear();
+}
+
+void getAt(ArrayList<int> &v)
+{
+	int index;
+	bool ok = true;
+	cout << "Write the index of the array" << endl;
+	cin >> index;
+	if (index < 0)
+	{
+		cout << "Index could not be negative" << endl;
+		ok = false;
+	}
+	if (index > (v.size() - 1))
+	{
+		cout << "Index out of size. The size is " << v.size() << endl;
+		ok = false;
+	}
+
+	if (ok)
+	{
+		cout << "The value is " << v.getAt(index) << endl;
+	}
+}
+
+void contains(ArrayList<int> &v)
+{
+	int searched;
+	cout << "Write the number that you want to search " << endl;
+	cin >> searched;
+	if (v.contains(searched))
+	{
+		cout << "The number " << searched << " is in the array " << endl;
+	}
+	else
+	{
+		cout << "The number " << searched << " isn't in the array " << endl;
+	}
+}
+
+void size(ArrayList<int> &v)
+{
+	cout << v.size() << endl;
+}
+
+void printAr(ArrayList<int> &v)
+{
+	cout << "[" << v.toString() << "]" << endl;
 }
